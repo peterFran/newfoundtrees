7230907230e250b4ed3e219c5d91a94d05a3a59c
Peter Meckiffe <peterf.meckiffe@gmail.com>
2021-03-10 00:57:50 +0000

    Changed markers

--- newfoundtrees/src/components/NewFoundTreesMap.tsx
+++ newfoundtrees/src/components/NewFoundTreesMap.tsx
@@ -1,17 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import React from 'react'
-import {
-    GoogleMap,
-    Marker,
-    useJsApiLoader,
-} from '@react-google-maps/api'
+import { GoogleMap, Marker, useJsApiLoader } from '@react-google-maps/api'
+// import MarkerWithLabel from "@react-google-maps/lib/component/MarkerWithLabel";
 import mapStyles from './mapStyles'
-import { OwnedToken, ListedToken } from '../domain/Token';
-import reforestationIcon from '../assets/map-pin-reforestation.svg'
-import rewildingIcon from '../assets/map-pin-rewilding.svg'
+import { OwnedToken, ListedToken } from '../domain/Token'
+// import reforestationIcon from '../assets/map-pin-reforestation.svg'
+// import rewildingIcon from '../assets/map-pin-rewilding.svg'
 import ListedInfoWindow from './ListedInfoWindow'
-import OwnedInfoWindow from './OwnedInfoWindow';
-import { makeStyles } from '@material-ui/core';
+import OwnedInfoWindow from './OwnedInfoWindow'
+import { makeStyles } from '@material-ui/core'
 
 const mapContainerStyle = {
     maxWidth: '100vw',
@@ -19,7 +16,7 @@ const mapContainerStyle = {
     width: '100vw',
     maxHeight: '100vh',
     margin: 0,
-    overflow: 'hidden'
+    overflow: 'hidden',
 }
 
 const options = {
@@ -36,13 +33,28 @@ const center = {
 }
 
 const useStyles = makeStyles((theme) => ({
-    fader:{
+    fader: {
         pointerEvents: 'none',
-        height: '100vh', 
-        position: 'absolute',   
+        height: '100vh',
+        position: 'absolute',
         width: '100vw',
-        background: 'linear-gradient(rgba(45,13,69, 40) 0%, transparent 40%)'
+        background: 'linear-gradient(rgba(45,13,69, 40) 0%, transparent 40%)',
     },
+    box: {
+        textAlign: 'left',
+        alignSelf: 'flex-end',
+        justifySelf: 'flex-end',
+        backgroundColor: '#2D0D45',
+        color: 'white',
+        transform: 'translateX(50%)',
+        paddingLeft: 3,
+        paddingRight: 3,
+        padding: 2,
+        fontFamily: 'Teko',
+        borderTopLeftRadius:10,
+        fontWeight: 700,
+        zIndex: -10
+    }
 }))
 
 interface NewFoundTreesMapProps {
@@ -84,80 +96,113 @@ const NewFoundTreesMap = ({
             <title>New Found Trees</title>
             {!isLoaded ? null : (
                 <div>
-                <GoogleMap
-                    mapContainerStyle={mapContainerStyle}
-                    zoom={2}
-                    center={center}
-                    options={options}
-                    onLoad={onLoad}
-                    onUnmount={onUnmount}
-                >
-                                    <div className={styles.fader}></div>
+                    <GoogleMap
+                        mapContainerStyle={mapContainerStyle}
+                        zoom={2}
+                        center={center}
+                        options={options}
+                        onLoad={onLoad}
+                        onUnmount={onUnmount}
+                    >
+                        <div className={styles.fader}></div>
 
-                    {listedTokens.map((project) => (
-                        <Marker
-                            zIndex={5}
-                            key={project.details.name}
-                            position={{
-                                lat: project.details.coordinates.latitude,
-                                lng: project.details.coordinates.longitude,
-                            }}
-                            icon={{
-                                url:
-                                    project.details.category === 'reforestation'
-                                        ? reforestationIcon
-                                        : rewildingIcon,
-                                origin: new window.google.maps.Point(0, 0),
-                                anchor: new window.google.maps.Point(23, 23),
-                                scaledSize: new window.google.maps.Size(46, 46),
-                            }}
-                            onClick={() => {
-                                console.log(map?.getCenter())
-                                map?.setZoom(5)
-                                map?.panTo({
+                        {listedTokens.map((project) => (
+                            <Marker
+                                zIndex={5}
+                                key={project.details.name}
+                                position={{
                                     lat: project.details.coordinates.latitude,
                                     lng: project.details.coordinates.longitude,
-                                })
-                                console.log(map?.getCenter())
+                                }}
+                                icon={{
+                                    path: google.maps.SymbolPath.CIRCLE,
+                                    strokeColor: '#2D0D45',
+                                    fillColor: 'white',
+                                    fillOpacity: 1,
+                                    strokeWeight: 2,
+                                    labelOrigin: new window.google.maps.Point(
+                                        0,
+                                        2.2
+                                    ),
+                                    scale: 5,
+                                    
+                                }}
+                                label={{
+                                    text: project.details.name.toUpperCase(),
+                                    color: 'white',   
+                                    className: styles.box,
+                                    
+                                }}
+                                onClick={() => {
+                                    console.log(map?.getCenter())
+                                    map?.setZoom(5)
+                                    map?.panTo({
+                                        lat:
+                                            project.details.coordinates
+                                                .latitude,
+                                        lng:
+                                            project.details.coordinates
+                                                .longitude,
+                                    })
+                                    console.log(map?.getCenter())
 
-                                setSelected(project)
-                            }}
-                        />
-                    ))}
-                    {ownedTokens.map((project) => (
-                        <Marker
-                            zIndex={2}
-                            key={project.details.name}
-                            position={{
-                                lat: project.details.coordinates.latitude,
-                                lng: project.details.coordinates.longitude,
-                            }}
-                            icon={{
-                                url:
-                                    project.details.category === 'reforestation'
-                                        ? reforestationIcon
-                                        : rewildingIcon,
-                                origin: new window.google.maps.Point(0, 0),
-                                anchor: new window.google.maps.Point(23, 23),
-                                scaledSize: new window.google.maps.Size(46, 46),
-                            }}
-                            onClick={() => {
-                                map?.setZoom(5)
-                                map?.setCenter({
+                                    setSelected(project)
+                                }}
+                            />
+                        ))}
+                        {ownedTokens.map((project) => (
+                            <Marker
+                                zIndex={2}
+                                key={project.details.name}
+                                position={{
                                     lat: project.details.coordinates.latitude,
                                     lng: project.details.coordinates.longitude,
-                                })
-                                setSelected(project)
-                            }}
-                        />
-                    ))}
-                    {selected && mapType === 'listed' && (
-                        <ListedInfoWindow token={selected as ListedToken} onCloseClick={() => setSelected(null)}/>
-                    )}
-                                        {selected && mapType === 'owned' && (
-                        <OwnedInfoWindow token={selected as OwnedToken} onCloseClick={() => setSelected(null)}/>
-                    )}
-                </GoogleMap>
+                                }}
+                                icon={{
+                                    path: google.maps.SymbolPath.CIRCLE,
+                                    strokeColor: '#2D0D45',
+                                    fillColor: 'white',
+                                    fillOpacity: 1,
+                                    strokeWeight: 2,
+                                    labelOrigin: new window.google.maps.Point(
+                                        0,
+                                        2.2
+                                    ),
+                                    scale: 5,
+                                }}
+                                label={{
+                                    text: project.details.name.toUpperCase(),
+                                    color: 'white',   
+                                    className: styles.box,
+                                    
+                                }}
+                                onClick={() => {
+                                    map?.setZoom(5)
+                                    map?.setCenter({
+                                        lat:
+                                            project.details.coordinates
+                                                .latitude,
+                                        lng:
+                                            project.details.coordinates
+                                                .longitude,
+                                    })
+                                    setSelected(project)
+                                }}
+                            />
+                        ))}
+                        {selected && mapType === 'listed' && (
+                            <ListedInfoWindow
+                                token={selected as ListedToken}
+                                onCloseClick={() => setSelected(null)}
+                            />
+                        )}
+                        {selected && mapType === 'owned' && (
+                            <OwnedInfoWindow
+                                token={selected as OwnedToken}
+                                onCloseClick={() => setSelected(null)}
+                            />
+                        )}
+                    </GoogleMap>
                 </div>
             )}
         </>
