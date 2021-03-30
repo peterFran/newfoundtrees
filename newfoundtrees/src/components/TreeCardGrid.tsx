c47813eb6688e462a1bd459430e800a8fb4fc0af
Peter Meckiffe <peterf.meckiffe@gmail.com>
2021-03-30 08:38:07 +0100

    Fixed positioning bug for mobile

--- newfoundtrees/src/components/TreeCardGrid.tsx
+++ newfoundtrees/src/components/TreeCardGrid.tsx
@@ -19,7 +19,7 @@ const useStyles = makeStyles((theme) => {
             height: '100%',
             flex: 1,
             overflow: 'hidden',
-            justifyContent: 'space-between',
+            justifyContent: 'center',
             alignItems: 'space-between',
             flexDirection: 'row',
         },
